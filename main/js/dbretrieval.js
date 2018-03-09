e7de9af07102fe604b5ebcce62a0a7af87cd6be4
Simon Gray <simongraygit@gmail.com>
2018-03-08 20:33:19 -0500

    Reading Record displays past history.

--- main/js/dbretrieval.js
+++ main/js/dbretrieval.js
@@ -1,7 +1,7 @@
 // retreieve goes through the books and adds them to the select dropdown on the Logging Reading page
-function retreieve() {
-    var selectQueryBuilder = readingListScheme.readingListDataBase.getSchema().table('Book');
-    var q1 = readingListScheme.readingListDataBase.select(selectQueryBuilder.id, selectQueryBuilder.bookId, selectQueryBuilder.currentPage).from(selectQueryBuilder);
+function retreieveBooks() {
+    // var selectQueryBuilder = readingListScheme.readingListDataBase.getSchema().table('Book');
+    // var q1 = readingListScheme.readingListDataBase.select(selectQueryBuilder.id, selectQueryBuilder.bookId, selectQueryBuilder.currentPage).from(selectQueryBuilder);
     readingListScheme.readingListDataBase.select().from(readingListScheme.bookTable).exec().then(function (rows) {
         var myCell = [];
         var returnObj = {};
@@ -18,4 +18,19 @@ function retreieve() {
         readingListScheme.books = returnObj;
         $('#mySelect').append(_select.html());
     });
+    retreieveReading()
+}
+
+function retreieveReading() {
+
+    var selectQueryBuilder = readingListScheme.readingListDataBase.select().from(readingListScheme.readingRecordsTable).exec().then(function (rows) {
+        readingListScheme.numberOfReadingLogs = rows.length;
+        var history = $('<table></table>').addClass('history');
+        for (i = 0; i < rows.length; i++) {
+            var row = $('<tr></tr>').text('Book: ' + rows[i].bookTitle + ' page: ' + rows[i].currentPage);
+            history.append(row);
+        }
+        $('#history').html(history);
+    });
+    console.log(selectQueryBuilder);
 }
